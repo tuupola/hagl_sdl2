1f407824f640ace82ccd25f366ca5e3928a23861
Mika Tuupola <tuupola@appelsiini.net>
2019-09-07 23:59:16 +0300

    Clean up demo code

--- demo.c
+++ demo.c
@@ -7,7 +7,8 @@
 
 #include "copepod.h"
 #include "pps.h"
-#include "font8x8.h"
+#include "fonts/8x8.h"
+//#include "fonts/renew.h"
 
 uint32_t flush_callback(uint32_t interval, void *param)
 {
@@ -36,7 +37,7 @@ void polygon_demo()
     int16_t y4 = (rand() % 280) - 20; /* -20 ... 260 */
     uint16_t colour = rand() % 0xffff;
     int16_t vertices[10] = {x0, y0, x1, y1, x2, y2, x3, y3, x4, y4};
-    pod_polygon(5, vertices, colour);
+    pod_draw_polygon(5, vertices, colour);
 }
 
 void fill_polygon_demo()
@@ -53,7 +54,7 @@ void fill_polygon_demo()
     int16_t y4 = (rand() % 280) - 20; /* -20 ... 260 */
     uint16_t colour = rand() % 0xffff;
     int16_t vertices[10] = {x0, y0, x1, y1, x2, y2, x3, y3, x4, y4};
-    pod_fillpolygon(5, vertices, colour);
+    pod_fill_polygon(5, vertices, colour);
 }
 
 void circle_demo()
@@ -62,7 +63,7 @@ void circle_demo()
     int16_t y0 = (rand() % 280) - 20; /* -20 ... 260 */
     uint16_t r = (rand() % 40);
     uint16_t colour = rand() % 0xffff;
-    pod_circle(x0, y0, r, colour);
+    pod_draw_circle(x0, y0, r, colour);
 }
 
 void fill_circle_demo()
@@ -71,7 +72,7 @@ void fill_circle_demo()
     int16_t y0 = (rand() % 280) - 20; /* -20 ... 260 */
     uint16_t r = (rand() % 40);
     uint16_t colour = rand() % 0xffff;
-    pod_fillcircle(x0, y0, r, colour);
+    pod_fill_circle(x0, y0, r, colour);
 }
 
 void line_demo()
@@ -81,7 +82,7 @@ void line_demo()
     int16_t x1 = (rand() % 360) - 20; /* -20 ... 340 */
     int16_t y1 = (rand() % 280) - 20; /* -20 ... 260 */
     uint16_t colour = rand() % 0xffff;
-    pod_line(x0, y0, x1, y1, colour);
+    pod_draw_line(x0, y0, x1, y1, colour);
 }
 
 void rectangle_demo()
@@ -97,7 +98,7 @@ void rectangle_demo()
     int16_t x4 = (rand() % 360) - 20; /* -20 ... 340 */
     int16_t y4 = (rand() % 280) - 20; /* -20 ... 260 */
     uint16_t colour = rand() % 0xffff;
-    pod_rectangle(x0, y0, x1, y1, colour);
+    pod_draw_rectangle(x0, y0, x1, y1, colour);
 }
 
 void fill_rectangle_demo()
@@ -113,7 +114,7 @@ void fill_rectangle_demo()
     int16_t x4 = (rand() % 360) - 20; /* -20 ... 340 */
     int16_t y4 = (rand() % 280) - 20; /* -20 ... 260 */
     uint16_t colour = rand() % 0xffff;
-    pod_fillrectangle(x0, y0, x1, y1, colour);
+    pod_fill_rectangle(x0, y0, x1, y1, colour);
 }
 
 void put_character_demo()
@@ -123,7 +124,7 @@ void put_character_demo()
 
     uint16_t colour = rand() % 0xffff;
     char ascii = rand() % 127;
-    pod_putchar(ascii, x0, y0, colour, font8x8_basic);
+    pod_put_char(ascii, x0, y0, colour, font);
 }
 
 void put_text_demo()
@@ -132,7 +133,7 @@ void put_text_demo()
     int16_t y0 = (rand() % 280) - 20; /* -20 ... 260 */
 
     uint16_t colour = rand() % 0xffff;
-    pod_puttext("YO! MTV Raps", x0, y0, colour, font8x8_basic);
+    pod_put_text("YO! MTV raps.", x0, y0, colour, font);
 }
 
 void put_pixel_demo()
@@ -141,7 +142,7 @@ void put_pixel_demo()
     int16_t y0 = (rand() % 280) - 20; /* -20 ... 260 */
 
     uint16_t colour = rand() % 0xffff;
-    pod_putpixel(x0, y0, colour);
+    pod_put_pixel(x0, y0, colour);
 }
 
 void triangle_demo()
@@ -153,7 +154,7 @@ void triangle_demo()
     int16_t x2 = (rand() % 360) - 20; /* -20 ... 340 */
     int16_t y2 = (rand() % 280) - 20; /* -20 ... 260 */
     uint16_t colour = rand() % 0xffff;
-    pod_triangle(x0, y0, x1, y1, x2, y2, colour);
+    pod_draw_triangle(x0, y0, x1, y1, x2, y2, colour);
 }
 
 void fill_triangle_demo()
@@ -165,13 +166,13 @@ void fill_triangle_demo()
     int16_t x2 = (rand() % 360) - 20; /* -20 ... 340 */
     int16_t y2 = (rand() % 280) - 20; /* -20 ... 260 */
     uint16_t colour = rand() % 0xffff;
-    pod_filltriangle(x0, y0, x1, y1, x2, y2, colour);
+    pod_fill_triangle(x0, y0, x1, y1, x2, y2, colour);
 }
 
 int main()
 {
     pod_init();
-    pod_clip_window(0, 30, 319, 210);
+    pod_set_clip_window(0, 30, 319, 210);
     srand(time(0));
 
     uint32_t flush_delay = 1000 / 30; /* 30 fps */
@@ -187,29 +188,29 @@ int main()
 
     while (!quit) {
 
-        if (0 == demo % 12) {
+        if (0 == demo) {
             put_text_demo();
-        } else if (0 == demo % 11) {
+        } else if (1 == demo) {
             put_character_demo();
-        } else if (0 == demo % 10) {
+        } else if (2 == demo) {
             put_pixel_demo();
-        } else if (0 == demo % 9) {
+        } else if (3 == demo) {
             fill_triangle_demo();
-        } else if (0 == demo % 8) {
+        } else if (4 == demo) {
             triangle_demo();
-        } else if (0 == demo % 7) {
+        } else if (5 == demo) {
             fill_rectangle_demo();
-        } else if (0 == demo % 6) {
+        } else if (6 == demo) {
             rectangle_demo();
-        } else if (0 == demo % 5) {
+        } else if (7 == demo) {
             line_demo();
-        } else if (0 == demo % 4) {
+        } else if (8 == demo) {
             circle_demo();
-        } else if (0 == demo % 3) {
+        } else if (9 == demo) {
             fill_circle_demo();
-        } else if (0 == demo % 2) {
+        } else if (10 == demo) {
             polygon_demo();
-        } else if (0 == demo % 1) {
+        } else if (11 == demo) {
             fill_polygon_demo();
         }
 
@@ -221,89 +222,18 @@ int main()
             }
             if (event.type == SDL_KEYDOWN) {
                 current_pps = pps(true);
-                demo = demo + 1;
+                demo = (demo + 1) % 12;
                 printf("Demo is now: %d\n", demo);
 
             }
         }
 
-        //SDL_Delay(500);
     }
+
+    //SDL_Delay(5000);
 
     SDL_RemoveTimer(flush_id);
     SDL_RemoveTimer(pps_id);
     pod_destroy();
     return 0;
 }
-
-int xmain()
-{
-    uint16_t max_x = 320;
-    uint16_t max_y = 280;
-    uint16_t max_iters = 1028;
-
-    uint16_t n = 0;
-    uint16_t px;
-    uint16_t py;
-
-    double x;
-    double y;
-    double x0;
-    double y0;
-    double xx;
-    double yy;
-    double xtemp;
-
-    double zoom = 1.0;
-
-    clock_t start;
-    clock_t end;
-    double time_spent;
-
-    pod_init();
-
-    start = clock();
-
-    for (py = 1; py <= max_y; py++)  {
-        for (px = 1; px <= max_x; px++)  {
-            x0 = (((float) px) / ((float) max_x) - 0.5) / zoom * 3.0 - 0.7;
-            y0 = (((float) py) / ((float) max_y) - 0.5) / zoom * 3.0;
-            x = 0.0;
-            y = 0.0;
-            n = 0;
-
-            xx = x * x;
-            yy = y * y;
-
-            while ((x * x + y * y < 4) && (n != max_iters)) {
-                xtemp = x * x - y * y + x0;
-                y = 2.0 * x * y + y0;
-                x = xtemp;
-                n++;
-            }
-
-            // while ((xx + yy < 4) && (n != max_iters)) {
-            //     y = (x + x) * y + y0;
-            //     x = xx - yy + x0;
-
-            //     xx = x * x;
-            //     yy = y * y;
-            //     n++;
-            // }
-
-            if (n < max_iters) {
-                pod_putpixel(px, py, n);
-            } else {
-                pod_putpixel(px, py, 0);
-            }
-        }
-    }
-
-    end = clock();
-    time_spent = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("Generated in %g seconds.\n", time_spent);
-    pod_flush();
-
-    return 0;
-}
-
--- fonts/8x8.h
+++ fonts/8x8.h
@@ -1,9 +1,9 @@
-/** 
+/**
  * 8x8 monochrome bitmap fonts for rendering
  * Author: Daniel Hepper <daniel@hepper.net>
- * 
+ *
  * License: Public Domain
- * 
+ *
  * Based on:
  * // Summary: font8x8.h
  * // 8x8 monochrome bitmap fonts for rendering
@@ -14,13 +14,14 @@
  * //
  * // License:
  * //     Public Domain
- * 
+ *
  * Fetched from: http://dimensionalrift.homelinux.net/combuster/mos3/?p=viewsource&file=/modules/gfx/font8_8.asm
  **/
 
 // Constant: font8x8_basic
 // Contains an 8x8 font map for unicode points U+0000 - U+007F (basic latin)
-char font8x8_basic[128][8] = {
+const char font[129][8] = {
+    { 0x08, 0x08 }, // width, height
     { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},   // U+0000 (nul)
     { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},   // U+0001
     { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},   // U+0002
